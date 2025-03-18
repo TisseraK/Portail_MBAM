1b151ba841ce7d4983db30cde0f66b3d1f35d710
TisseraK <k.tisserand@outlook.fr>
2025-03-18 19:24:26 +0100

    ddd

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4022872771"
+    serviceWorkerVersion: "1744423409"
   }
 });
